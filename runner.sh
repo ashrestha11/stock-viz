b63d467d92391eb79825723a25e2b441794e72a6
ashrestha11 <shrestha.anurag7@gmail.com>
2021-07-31 11:02:36 -0700

    env variables

--- runner.sh
+++ runner.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-until ./viz/bin/python3 ./app/collector.py -config 'sheets_config.json' -n 'StockMentions' -s 'wallstreetbets+options+pennystocks' -w 'Raw'
+export ENV="./viz/bin/python3" # your python env w/ all package
+export GSHEET_CONFIG="sheets_config.json" # your google sheets api creds
+export SHEET_NAME="StockMentions"
+export SUBREDDITS="wallstreetbets+options+pennystocks" # which stocks to track
+export WORKSHEET="Raw"
+
+until $ENV ./app/collector.py -config $GSHEET_CONFIG -n $SHEET_NAME -s $SUBREDDITS -w $WORKSHEET
 do
     echo "Restarting"
     sleep 10
 done
+
